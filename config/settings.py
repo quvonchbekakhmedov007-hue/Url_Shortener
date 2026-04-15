86f99e57de76004af760605192fcee6e8ae70de0
Quvonchbek Axmedov <quvonchbekakhmedov007@gmail.com>
2026-04-15 21:31:26 +0500

    feat: add redirect view for URL shortener

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'shortener',
 ]
 
@@ -117,3 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+BASE_URL = "http://localhost:8000"
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'URL Shortener API',
+    'DESCRIPTION': 'Bitly clone',
+    'VERSION': '1.0.0',
+}
